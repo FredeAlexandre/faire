77c8583e468c5b26d051b86f046fac2635923bb8
Alexandre Frede <alexandre.frede.pro@proton.me>
2024-09-22 14:10:52 +0200

    feat: add active routes ui

--- apps/web-app/src/components/floating-navbar.tsx
+++ apps/web-app/src/components/floating-navbar.tsx
@@ -2,6 +2,7 @@
 
 import type { LucideIcon } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import {
   Calendar,
   Inbox,
@@ -14,6 +15,7 @@ import {
 } from "lucide-react";
 import { useTheme } from "next-themes";
 
+import { cn } from "@faire/ui";
 import { Button } from "@faire/ui/button";
 import {
   Tooltip,
@@ -63,12 +65,19 @@ function NavbarButton({
   label: string;
   to: string;
 }) {
+  const currentPath = usePathname();
+
+  const active = currentPath == to;
+
   return (
     <TooltipProvider>
       <Tooltip>
         <TooltipTrigger asChild>
           <Button
-            className="h-12 w-12 rounded-full [&.active]:bg-primary [&.active]:text-primary-foreground [&.active]:shadow [&.active]:hover:bg-primary/90"
+            className={cn(
+              { active },
+              "h-12 w-12 rounded-full [&.active]:bg-primary [&.active]:text-primary-foreground [&.active]:shadow [&.active]:hover:bg-primary/90",
+            )}
             variant="ghost"
             asChild
           >
@@ -88,7 +97,7 @@ function NavbarButton({
 export function FloatingNavbar() {
   return (
     <div className="fixed bottom-8 left-1/2 z-50 flex -translate-x-1/2 gap-1 rounded-full border bg-background p-1 shadow-sm">
-      <NavbarButton to="/" icon={Inbox} label="Inbox" />
+      <NavbarButton to="/inbox" icon={Inbox} label="Inbox" />
       <NavbarButton to="/execute" icon={Zap} label="Execute" />
       <NavbarButton to="/actions" icon={ListTree} label="Actions" />
       <NavbarButton to="/events" icon={Calendar} label="Events" />
